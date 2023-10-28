4e8d1b9c1585279813f928df2fcf684fa70efdb0
Minnowo <62451415+Minnowo@users.noreply.github.com>
2023-10-28 15:00:20 -0400

    fixed async bug in context dialog

--- dmus/lib/ui/dialogs/SongContextDialog.dart
+++ dmus/lib/ui/dialogs/SongContextDialog.dart
@@ -1,9 +1,8 @@
 import 'package:dmus/core/audio/AudioMetadata.dart';
-import 'package:dmus/core/data/FileDialog.dart';
-import 'package:dmus/core/data/MusicFetcher.dart';
 import 'package:dmus/ui/pages/MetadataPage.dart';
 import 'package:flutter/material.dart';
 
+import '../../core/Util.dart';
 import '../../core/data/DataEntity.dart';
 
 class SongContextDialog extends StatelessWidget {
@@ -11,6 +10,32 @@ class SongContextDialog extends StatelessWidget {
   final Song songContext;
 
   const SongContextDialog ({required this.songContext, super.key});
+  
+  Future<void> showMetadataPage(BuildContext context) async {
+
+    if(songContext.file.path == null) {
+
+      logging.warning("songContext has no valid file path!");
+
+      Navigator.pop(context);
+
+      return;
+    }
+
+    logging.finest("Fetching songs metadata.");
+
+    // TODO: put a snackbar popup or some indicator here, this may take some time
+    var m = await getMetadata(songContext.file.path!);
+
+    // https://dart.dev/tools/linter-rules/use_build_context_synchronously
+    if (!context.mounted)  {
+      logging.warning("BuildContext was not mounted, the widget was destroyed");
+      return;
+    }
+
+    Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (ctxt) => MetadataPage(metadata: m,)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,18 +45,7 @@ class SongContextDialog extends StatelessWidget {
         children: <Widget>[
           ListTile(
             title: const Text('View Details'),
-            onTap: () async {
-              // Handle Option 1
-              Navigator.pop(context);
-
-              if(songContext.file.path == null)
-                return;
-
-
-              var m = await getMetadata(songContext.file.path!);
-
-              Navigator.push(context,MaterialPageRoute(builder: (ctxt) => MetadataPage(metadata: m,)));
-            },
+            onTap: () => showMetadataPage(context),
           ),
           ListTile(
             title: Text('Close'),
