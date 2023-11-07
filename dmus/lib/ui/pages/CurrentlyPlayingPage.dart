e22ee06cf68e1e0b5f3e825b07e2e8d6ddd9756a
Edmond <edmond.lee@ontariotechu.net>
2023-11-07 13:51:07 -0500

    UI changes to the UI in the Currently PLaying Page, need to fix flickering when you press the faviroute button

--- dmus/lib/ui/pages/CurrentlyPlayingPage.dart
+++ dmus/lib/ui/pages/CurrentlyPlayingPage.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 import 'dart:io';
 
@@ -13,48 +12,38 @@ import '../../core/Util.dart';
 import '../../core/audio/AudioController.dart';
 import '../../core/data/DataEntity.dart';
 
-
 class CurrentlyPlayingPage extends StatefulWidget {
   const CurrentlyPlayingPage({super.key});
 
   @override
-  State<StatefulWidget> createState () => CurrentlyPlayingPageState();
+  State<StatefulWidget> createState() => CurrentlyPlayingPageState();
 }
 
-
 class CurrentlyPlayingPageState extends State<CurrentlyPlayingPage> {
-
   static const String title = "Currently Playing";
+  bool isLiked = false;
 
   late final StreamSubscription<Song?> currentlyPlayingSubscriber;
 
   Song? songContext;
 
-
-
   void _onSongChanged(Song? s) {
-
-    if(s == null || s == songContext) {
+    if (s == null || s == songContext) {
       return;
     }
-
     setState(() {
       songContext = s;
     });
   }
 
-
   @override
   void initState() {
     super.initState();
-
     currentlyPlayingSubscriber = AudioController.onSongChanged.listen(_onSongChanged);
-
     setState(() {
       songContext = context.read<AudioControllerModel>().currentlyPlaying;
     });
   }
-
 
   @override
   void dispose() {
@@ -62,80 +51,88 @@ class CurrentlyPlayingPageState extends State<CurrentlyPlayingPage> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
-
-    if(songContext == null) {
+    if (songContext == null) {
       return const CircularProgressIndicator();
     }
 
     logging.info("Currently playing page now showing $songContext");
 
     return Scaffold(
-
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text(title),
         actions: [
           IconButton(
             icon: const Icon(Icons.more_vert),
-            onPressed: () {
-            },
+            onPressed: () {},
           ),
         ],
       ),
-
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-
-          if(songContext!.metadata.albumArt != null)
+          if (songContext!.metadata.albumArt != null)
             Image.memory(songContext!.metadata.albumArt!),
-
-          if(songContext!.metadata.albumArt == null && songContext!.pictureCacheKey != null)
+          if (songContext!.metadata.albumArt == null && songContext!.pictureCacheKey != null)
             FutureBuilder<File?>(
               future: ImageCacheController.getImagePathFromRaw(songContext!.pictureCacheKey!),
               builder: (BuildContext context, AsyncSnapshot<File?> snapshot) {
-
-                if (snapshot.connectionState != ConnectionState.done ) {
+                if (snapshot.connectionState != ConnectionState.done) {
                   return const CircularProgressIndicator();
                 }
-
                 if (snapshot.hasError) {
                   return Text('Error: ${snapshot.error}');
                 }
-
-                if(!snapshot.hasData) {
+                if (!snapshot.hasData) {
                   return const CircularProgressIndicator();
                 }
-
                 if (snapshot.data != null) {
-                  return Image.file(snapshot.data!, fit: BoxFit.cover, );
+                  return Image.file(
+                    snapshot.data!,
+                    fit: BoxFit.cover,
+                  );
                 }
-
                 return const Text('No image path found.');
               },
             ),
-
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Text( songContext!.title ,
+            child: Text(
+              songContext!.title,
               style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
-
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               IconButton(
-                icon: const Icon(Icons.favorite_border),
+                icon: Icon(
+                  // Conditional icon based on the liked state
+                  isLiked ? Icons.favorite : Icons.favorite_border,
+                  // Conditional color based on the liked state
+                  color: isLiked ? Colors.red : null,
+                ),
                 onPressed: () {
+                  setState(() {
+
+                    isLiked = !isLiked;
+
+
+                    if (isLiked) {
+                     logging.finest("SONG FAVOURITED");
+                    } else {
+                      logging.finest("SONG UNFAVOURITED");
+
+                    }
+                  });
                 },
               ),
               IconButton(
                 icon: const Icon(Icons.playlist_add),
                 onPressed: () {
+                  logging.finest("ADD TO PLAYLIST");
                 },
               ),
             ],
@@ -143,56 +140,122 @@ class CurrentlyPlayingPageState extends State<CurrentlyPlayingPage> {
 
           Consumer<AudioControllerModel>(
             builder: (context, audioControllerModel, child) {
-
               final songDuration = audioControllerModel.duration;
               final songPosition = audioControllerModel.position;
-
               return Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        if(!audioControllerModel.isPlaying)
-                          IconButton(
-                            icon: const Icon(Icons.play_arrow),
-                            onPressed: () async { await AudioController.resume(); },
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.black,
                           ),
-
-                        if(audioControllerModel.isPlaying)
-                          IconButton(
-                            icon: const Icon(Icons.pause),
-                            onPressed: () async { await AudioController.pause(); },
+                          child: Center(
+                            child: Icon(
+                              Icons.skip_previous,
+                              size: 30,
+                              color: Colors.white,
+                            ),
                           ),
-                        IconButton(
-                          icon: const Icon(Icons.stop),
-                            onPressed: () async { await AudioController.stop(); },
                         ),
-                      ],
-                    ),
+                        onPressed: () async {
+                         logging.finest("PREVIOUS SONG");
+                        },
+                      ),
+                      IconButton(
+                        icon: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.black,
+                          ),
+                          child: Center(
+                            child: Icon(
+                              audioControllerModel.isPlaying
+                                  ? Icons.pause
+                                  : Icons.play_arrow,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        onPressed: () async {
+                          if (audioControllerModel.isPlaying) {
+                            await AudioController.pause();
+                          } else {
+                            await AudioController.resume();
+                          }
+                        },
+                      ),
+                      IconButton(
+                        icon: Container(
+                          width: 50, // Adjust the size of the circle as needed
+                          height: 50, // Adjust the size of the circle as needed
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.black, // Change the color of the circle
+                          ),
+                          child: Center(
+                            child: Icon(
+                              Icons.stop, // Display the stop icon
+                              size: 30, // Adjust the size of the stop icon as needed
+                              color: Colors.white, // Change the color of the icon
+                            ),
+                          ),
+                        ),
+                        onPressed: () async {
+                          await AudioController.stop();
+                        },
+                      ),
+                      IconButton(
+                        icon: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.black,
+                          ),
+                          child: Center(
+                            child: Icon(
+                              Icons.skip_next,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        onPressed: () async {
+                          logging.finest(" NEXT SONG");
+                        },
+                      ),
+                    ],
+                  ),
 
-                    TimeSlider(songDuration: songDuration, songPosition: songPosition)
-                  ]
+                  TimeSlider(songDuration: songDuration, songPosition: songPosition),
+                ],
               );
             },
           ),
-
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               IconButton(
                 icon: const Icon(Icons.shuffle),
-                onPressed: () {
-                },
+                onPressed: () {},
               ),
               IconButton(
                 icon: const Icon(Icons.repeat),
-                onPressed: () {
-                },
+                onPressed: () {},
               ),
               IconButton(
                 icon: const Icon(Icons.speed),
                 onPressed: () {
-                  showDialog(context: context, builder: (ctx) => const SpeedModifierPicker()) ;
+                  showDialog(context: context, builder: (ctx) => const SpeedModifierPicker());
                 },
               ),
             ],
