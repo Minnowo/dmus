ee1dafe636ba8de72ed5b456964f8005f2fbb7b4
samuel-shard <59847990+samuel-shard@users.noreply.github.com>
2023-11-30 11:57:17 -0500

    removed unnecessary comments on ThemeToggle.dart

--- dmus/lib/ui/widgets/ThemeToggle.dart
+++ dmus/lib/ui/widgets/ThemeToggle.dart
@@ -1,30 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// //Stateful widget to be added to the settings drawer to save the dark mode setting
-// class ThemeToggle extends StatefulWidget {
-//   final ValueChanged<bool> onToggle;
-//   final bool isDarkModeEnabled;
-
-//   const ThemeToggle({Key? key, required this.onToggle, required this.isDarkModeEnabled}) : super(key: key);
-
-//   @override
-//   _ThemeToggleState createState() => _ThemeToggleState();
-// }
-
-// class _ThemeToggleState extends State<ThemeToggle> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return SwitchListTile(
-//       title: const Text('Dark Mode'),
-//       value: widget.isDarkModeEnabled,
-//       onChanged: (value) {
-//         widget.onToggle(value);
-//       },
-//     );
-//   }
-// }
-
-
 import 'package:dmus/ui/widgets/ThemeProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
