480c7d7c949788ac95fa041c20b6240417de0459
Edmond <edmond.lee@ontariotechu.net>
2023-12-01 14:41:40 -0500

    removed inkwell from songtile

--- dmus/lib/ui/pages/SongsPage.dart
+++ dmus/lib/ui/pages/SongsPage.dart
@@ -80,18 +80,15 @@ class SongsPage extends  StatelessNavigationPage {
                       child: ListView (
                         children: [
                           for(final i in songsProvider.songs)
-                            InkWell(
-                              borderRadius: BorderRadius.circular(100.0),
-                              child: SongListWidget(
-                                song: i,
-                                leadWith: SongListWidgetLead.leadWithArtwork,
-                                trailWith: SongListWidgetTrail.trailWithMenu,
-                                confirmDismiss: (d) => addToQueueSongDismiss(d, i),
-                                onTap: () => JustAudioController.instance.playSong(i),
-                                onLongPress: () => SongContextDialog.showAsDialog(context, i),
-                                selected: false,
-                                background: iconDismissibleBackgroundContainer(Colors.green, Icons.queue),
-                              ),
+                            SongListWidget(
+                              song: i,
+                              leadWith: SongListWidgetLead.leadWithArtwork,
+                              trailWith: SongListWidgetTrail.trailWithMenu,
+                              confirmDismiss: (d) => addToQueueSongDismiss(d, i),
+                              onTap: () => JustAudioController.instance.playSong(i),
+                              onLongPress: () => SongContextDialog.showAsDialog(context, i),
+                              selected: false,
+                              background: iconDismissibleBackgroundContainer(Colors.green, Icons.queue),
                             )
                         ],
                       )
